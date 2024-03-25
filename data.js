24a89fe2b041d3c72e7b249a91322fc1622da294
Duru Aydoğdu <119884124+duruaydogdu@users.noreply.github.com>
2024-03-25 20:20:06 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const projectsData = [
+    {
+      title: 'Chat App',
+      images: './assests/projects/chat-app.jpg',
+      appLink: 'https://github.com/isinnur/react-firebase-chat-app',
+    },
+    {
+      title: 'Blog App',
+      images: './assests/projects/blog-app.jpg',
+      appLink: 'https://github.com/isinnur/fullstack-blog-app',
+    },
+    {
+      title: 'Movie App',
+      images: './assests/projects/movie-app.jpg',
+      appLink: 'https://github.com/isinnur/nextjs-movieapp',
+    },
+    {
+      title: 'Valorant Game App',
+      images: './assests/projects/valorant.jpg',
+      appLink: 'https://github.com/isinnur/valorant-game-app',
+    },
+  ];
